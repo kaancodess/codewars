f3a07d7afb54425399c7e9c3b71668499c0ca101
kaancodess <kaancodes@gmail.com>
2023-12-26 21:15:14 +0200

    Kata Example Twist problem solution

---
+++ solved/kata_example_twist.py
@@ -0,0 +1,9 @@
+websites = []
+
+how_many = len(websites)
+
+while how_many < 1000:
+	websites.append("codewars")
+	how_many += 1
+
+# solution for https://www.codewars.com/kata/525c1a07bb6dda6944000031/train/python
