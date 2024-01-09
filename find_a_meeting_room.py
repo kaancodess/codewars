215f96fbb8641a1795ac9801b82dca4bbd62e6b4
kaancodess <112093775+kaancodess@users.noreply.github.com>
2024-01-09 13:06:16 +0200

    Delete find_a_meeting_room.py

--- find_a_meeting_room.py
+++
@@ -1,9 +0,0 @@
-def meeting(rooms):
-    for o in rooms:
-        if o == "O":
-            empty_room = o
-            return rooms.index(empty_room)
-    else:
-        return 'None available!'
-    
-# solution for https://www.codewars.com/kata/57f604a21bd4fe771b00009c/train/python
