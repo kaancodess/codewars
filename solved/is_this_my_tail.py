6bfe29f75e184f6b7e876f3670d09a3c52b4f662
kaancodess <112093775+kaancodess@users.noreply.github.com>
2024-01-28 22:45:46 +0200

    Is this my tail solution added

---
+++ solved/is_this_my_tail.py
@@ -0,0 +1,8 @@
+def correct_tail(body, tail):
+    sub = body[-1]
+    if sub == tail:
+        return True
+    else:
+        return False
+
+# solution for https://www.codewars.com/kata/56f695399400f5d9ef000af5/train/python
