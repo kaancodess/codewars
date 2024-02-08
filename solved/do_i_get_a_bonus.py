1d61524a9e8b9bf98b4cc13770d91bb9839349e6
kaancodess <112093775+kaancodess@users.noreply.github.com>
2024-02-08 20:59:43 +0200

    Do i get a bonus solution added

---
+++ solved/do_i_get_a_bonus.py
@@ -0,0 +1,7 @@
+def bonus_time(salary, bonus):
+    if bonus == True:
+        return str(f"${salary * 10}")
+    else:
+        return str(f"${salary}")
+
+# solution for https://www.codewars.com/kata/56f6ad906b88de513f000d96/train/python
