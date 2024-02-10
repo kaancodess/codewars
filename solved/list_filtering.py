05237975cc52f40066a57a7633c180f49304ac42
kaancodess <112093775+kaancodess@users.noreply.github.com>
2024-02-10 18:08:02 +0200

    List filtering solution added

---
+++ solved/list_filtering.py
@@ -0,0 +1,9 @@
+def filter_list(l):
+    lst = []
+    for x in l:
+        if type(x) == int:
+            lst.append(x)
+            
+    return lst
+
+# solution for https://www.codewars.com/kata/53dbd5315a3c69eed20002dd/train/python
