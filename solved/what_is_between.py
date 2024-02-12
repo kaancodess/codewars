a63f3c67ffb509ffc7b38ca90504ff7374ab676f
kaancodess <112093775+kaancodess@users.noreply.github.com>
2024-02-13 00:03:53 +0200

    What is between solution added

---
+++ solved/what_is_between.py
@@ -0,0 +1,7 @@
+def between(a,b):
+    lst = []
+    for x in range(a,b + 1):
+        lst.append(x)
+    return lst
+
+# solution for https://www.codewars.com/kata/55ecd718f46fba02e5000029/train/python
