ab4823b67704eed8b551b0e8629fbf55fd3fcfd4
kaancodess <112093775+kaancodess@users.noreply.github.com>
2024-02-14 22:24:19 +0200

    Summation solution added

---
+++ solved/summation.py
@@ -0,0 +1,9 @@
+def summation(num):
+    lst = []
+    for x in range(1,num + 1):
+        lst.append(x)
+    
+    total = sum(lst)
+    return total
+
+#solution for https://www.codewars.com/kata/55d24f55d7dd296eb9000030/train/python
