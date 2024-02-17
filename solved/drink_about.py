885d7d95d1180b90abcd05f4b51d4653cf42f247
kaancodess <112093775+kaancodess@users.noreply.github.com>
2024-02-17 23:40:19 +0200

    Drink about solution added

---
+++ solved/drink_about.py
@@ -0,0 +1,15 @@
+def people_with_age_drink(age):
+    
+    if age < 14:
+        return "drink toddy"
+    
+    elif age >= 14 and age < 18:
+        return "drink coke"
+    
+    elif age >= 18 and age < 21:
+        return "drink beer"
+    
+    elif age >= 21:
+        return "drink whisky"
+
+# solution for https://www.codewars.com/kata/56170e844da7c6f647000063/train/python
