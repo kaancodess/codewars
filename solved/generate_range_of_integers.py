1ddd3f8e2f79fa7953d7c3563309b8e7838b0a92
kaancodess <112093775+kaancodess@users.noreply.github.com>
2024-02-18 22:36:40 +0200

    Generate range of integers solution added

---
+++ solved/generate_range_of_integers.py
@@ -0,0 +1,8 @@
+def generate_range(start, stop, step):
+    lst = []
+    for x in range(start,stop + 1,step):
+        lst.append(x)
+        
+    return lst
+
+# solution for https://www.codewars.com/kata/55eca815d0d20962e1000106/train/python
