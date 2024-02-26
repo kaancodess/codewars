01fb0545acd483a34179f0d03d705d67cb1964f9
kaancodess <112093775+kaancodess@users.noreply.github.com>
2024-02-26 23:33:31 +0200

    Pirates are the cannons ready ? solution added

---
+++ solved/pirates_are_the_cannons_ready.py
@@ -0,0 +1,9 @@
+def cannons_ready(gunners):
+    x = gunners.values()
+    txt = 'nay'
+    if txt in x:
+        return "Shiver me timbers!"
+    else:
+        return "Fire!"
+
+# solution for https://www.codewars.com/kata/5748a883eb737cab000022a6/train/python
