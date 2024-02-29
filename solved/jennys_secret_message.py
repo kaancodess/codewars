34b9ad2b4a98ca5f0c5f6d34aec60d00c5b56a4b
kaancodess <112093775+kaancodess@users.noreply.github.com>
2024-02-29 23:16:55 +0200

    Jenny's secret message solution added

---
+++ solved/jennys_secret_message.py
@@ -0,0 +1,7 @@
+def greet(name):
+    if name == "Johnny":
+        return "Hello, my love!"
+    else:
+        return f"Hello, {name}!".format(name != "Johnny")
+
+# solution for https://www.codewars.com/kata/55225023e1be1ec8bc000390/train/python
