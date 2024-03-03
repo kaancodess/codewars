fadf64b61ee5bb240699a7d5c9181339ca574f1d
kaancodess <112093775+kaancodess@users.noreply.github.com>
2024-03-03 22:49:37 +0200

    Counting sheep solution added

---
+++ solved/counting_sheep.py
@@ -0,0 +1,9 @@
+def count_sheeps(sheep):
+    count = 0
+    for x in sheep:
+        if x == True:
+            count += 1
+            
+    return count
+
+# solution for https://www.codewars.com/kata/54edbc7200b811e956000556/train/python
