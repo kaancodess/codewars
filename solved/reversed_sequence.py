5324c9d048b7de14e73cf0ef9489cd56292bba3c
kaancodess <kaancodes@gmail.com>
2024-03-14 19:00:23 +0200

    Reversed Sequence solution added

---
+++ solved/reversed_sequence.py
@@ -0,0 +1,8 @@
+def reverse_seq(n):
+    lst = []
+    for x in range(1,n + 1):
+        lst.append(x)
+        
+    return lst[::-1]
+
+# solution for https://www.codewars.com/users/bl1tzkr13g/completed_solutions
