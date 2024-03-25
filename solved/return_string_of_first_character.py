744e9436967591d956af763f21aa55e079bbc1a8
kaancodess <kaancodes@gmail.com>
2024-03-25 22:58:37 +0200

    Return String Of a First Character solution added

---
+++ solved/return_string_of_first_character.py
@@ -0,0 +1,10 @@
+text  = "This Is A Test"
+lst = text.split(" ")
+print(lst)
+result = ""
+for x in lst:
+    result += x[0]
+
+print(result)
+
+# solution for https://www.codewars.com/kata/5639bdcef2f9b06ce800005b/train/python
